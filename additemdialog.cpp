000caef0d247abbb92c323991c7082dcb512abad
Nikita Kazeev <kazeevn@gmail.com>
2012-05-15 03:03:40 +0400

    Added adding lens.

--- additemdialog.cpp
+++ additemdialog.cpp
@@ -4,6 +4,7 @@
 #include "additemdialog.h"
 #include "ui_additemdialog.h"
 #include "core/objects/stubs/pictureobjectstub.h"
+#include "core/objects/stubs/lensobjectstub.h"
 #include "core/models/model.h"
 #include "core/geometry/plane3d.h"
 #include "core/geometry/vector3d.h"
@@ -16,8 +17,8 @@ AddItemDialog::AddItemDialog(MainWindow *parent) :
     ui->setupUi(this);    
     // TODO(kazeevn) make it python-style for item in {...}
     ui->listWidget->addItem("Picture");
-    ui->listWidget->addItem("Lense");
-    ui->listWidget->addItem("Ideal Lense");
+    ui->listWidget->addItem("Lens");
+    ui->listWidget->addItem("Ideal Lens");
     ui->listWidget->addItem("Flat Mirror");
 
     current_object = new PictureObjectStub(Point3D(0,0,0),Vector3D(0,0,0), Vector3D(0,0,0), QImage());
@@ -41,7 +42,7 @@ void AddItemDialog::selectionChangedSlot(const QItemSelection & newSelection, co
         ui->doubleSpinBox_2->setEnabled(false);
         ui->label_1->setText("");
         ui->label_2->setText("");
-    } else if (selected=="Lense") {
+    } else if (selected=="Lens") {
         ui->doubleSpinBox_1->setEnabled(true);
         ui->doubleSpinBox_2->setEnabled(true);
         ui->label_1->setText("Height");
@@ -63,6 +64,18 @@ void AddItemDialog::on_pushButtonOK_clicked()
         Scene::Instance().addStubObject(ui->editName->text(), current_object);
         m_window->refresh();
         this->close();
+    } else if (selected=="Lens") {
+        LensObjectStub* lens_object = new LensObjectStub(current_object->point(), current_object->v1(), current_object->v2(),
+                                         m_image,m_image_rear,QSize(ui->spinBox->value(),ui->spinBox_2->value()),
+                                         ui->spinBox->value(),ui->spinBox_2->value());
+        if (!current_object->isValid())
+        {
+            delete lens_object;
+            return;
+        }
+        Scene::Instance().addStubObject(ui->editName->text(), lens_object);
+        m_window->refresh();
+        this->close();
     }
 }
 
@@ -74,4 +87,13 @@ void AddItemDialog::on_loadImage_clicked()
     out_image.addPixmap(QPixmap::fromImage(m_image));
     ui->graphicsView->setScene(&out_image);
     current_object->setImage(m_image);
+}
+
+void AddItemDialog::on_loadImage2_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Save Image"), "", tr("Image Files (*.png *.jpg *.bmp)"));
+    m_image_rear.load(fileName);
+    out_image_rear.addPixmap(QPixmap::fromImage(m_image_rear));
+    ui->graphicsView_2->setScene(&out_image_rear);
 }
--- additemdialog.h
+++ additemdialog.h
@@ -25,13 +25,16 @@ private:
     Ui::AddItemDialog *ui;
     PictureObjectStub* current_object;
     QImage m_image;
+    QImage m_image_rear;
     QGraphicsScene out_image;
+    QGraphicsScene out_image_rear;
     MainWindow* m_window;
 
 private slots:
     void selectionChangedSlot(const QItemSelection & newSelection, const QItemSelection & oldSelection);
     void on_pushButtonOK_clicked();
     void on_loadImage_clicked();
+    void on_loadImage2_clicked();
 };
 
 #endif // ADDITEMDIALOG_H
